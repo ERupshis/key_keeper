91d29c4ecda692fd5e33231fd374f439e4caa9bd
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2024-01-14 14:19:11 +0200

    client. inmemory. get record refactoring

--- internal/agent/storage/inmemory/get.go
+++ internal/agent/storage/inmemory/get.go
@@ -15,29 +15,38 @@ func (s *Storage) GetRecord(id int64) (*data.Record, error) {
 }
 
 func (s *Storage) GetRecords(recordType data.RecordType, filters map[string]string) ([]data.Record, error) {
-	scanAllTypes := recordType == data.TypeAny
-
 	var res []data.Record
-	for _, rec := range s.records {
-		if rec.RecordType != recordType && !scanAllTypes {
+	for idx := range s.records {
+		if !canRecordBeReturned(&s.records[idx], recordType, filters) {
 			continue
 		}
 
-		if len(filters) == 0 {
-			res = append(res, rec)
-			continue
-		}
-
-		if isRecordMatchToFilters(&rec, filters) {
-			res = append(res, rec)
+		if isRecordMatchToFilters(&s.records[idx], filters) {
+			res = append(res, s.records[idx])
 		}
 	}
 
 	return res, nil
 }
 
+func canRecordBeReturned(record *data.Record, recordType data.RecordType, filters map[string]string) bool {
+	if record.Deleted {
+		return false
+	}
+
+	if record.RecordType != recordType && !(recordType == data.TypeAny) {
+		return false
+	}
+
+	return true
+}
+
 func isRecordMatchToFilters(record *data.Record, filters map[string]string) bool {
 	match := true
+	if len(filters) == 0 {
+		return true
+	}
+
 	for key, val := range filters {
 		if key == data.StrAny {
 			match = isSomeRecordMetaDataHasValue(record, val)
