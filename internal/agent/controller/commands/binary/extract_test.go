6dabbbc6f1a1c5f2eb9a95db567080bd9b900bde
Edgar Rupshis <73851167+ERupshis@users.noreply.github.com>
2024-02-10 12:27:53 +0200

    tests. controller. binary. part 2

--- internal/agent/controller/commands/binary/extract_test.go
+++ internal/agent/controller/commands/binary/extract_test.go
@@ -1,11 +1,15 @@
 package binary
 
 import (
+	"bytes"
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/erupshis/key_keeper/internal/agent/controller/commands/statemachines"
 	"github.com/erupshis/key_keeper/internal/agent/interactor"
+	"github.com/erupshis/key_keeper/internal/agent/models"
+	"github.com/erupshis/key_keeper/internal/agent/utils/testutils"
 	"github.com/erupshis/key_keeper/internal/common/crypt/ska"
 	"github.com/erupshis/key_keeper/internal/common/hasher"
 	"github.com/erupshis/key_keeper/internal/common/logger"
@@ -203,6 +207,138 @@ func TestBinary_decryptFileAndValidate(t *testing.T) {
 				return
 			}
 			assert.Equalf(t, tt.want.fileBytes, got, "decryptFileAndValidate(%v, %v)", tt.args.fileBytes, tt.args.checkSum)
+		})
+	}
+}
+
+func TestBinary_saveFile(t *testing.T) {
+	type fields struct {
+		sm        *statemachines.StateMachines
+		hash      *hasher.Hasher
+		cryptor   *ska.SKA
+		rd        *bytes.Reader
+		wr        *bytes.Buffer
+		storePath string
+	}
+	type args struct {
+		record     *models.Record
+		fileBytes  []byte
+		pathToFile string
+	}
+	type want struct {
+		response  []byte
+		fileBytes []byte
+		err       assert.ErrorAssertionFunc
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   want
+	}{
+		{
+			name: "base",
+			fields: fields{
+				cryptor: ska.NewSKA("pass", ska.Key16),
+				hash:    hasher.CreateHasher("", hasher.TypeSHA256, logger.CreateMock()),
+				rd:      bytes.NewReader([]byte(testutils.AddNewRow("") + testutils.AddNewRow(""))),
+				wr:      bytes.NewBuffer(nil),
+			},
+			args: args{
+				record:    &models.Record{Data: models.Data{RecordType: models.TypeBinary, Binary: &models.Binary{Name: "saveFile_base.txt", SecuredFileName: "9a30a503b2862c51c3c5acd7fbce2f1f784cf4658ccf8e87d5023a90c21c0714"}}},
+				fileBytes: []byte("0sAy8Kq2v5AQtBaeGXahfBV1lPpzp3Ob4HITwxMARgs="),
+			},
+			want: want{
+				response:  []byte("file extracted: saveFile_base.txt\n"),
+				fileBytes: []byte("test file"),
+				err:       assert.NoError,
+			},
+		},
+		{
+			name: "invalid cryptor key",
+			fields: fields{
+				cryptor: ska.NewSKA("wrong", ska.Key16),
+				hash:    hasher.CreateHasher("", hasher.TypeSHA256, logger.CreateMock()),
+				rd:      bytes.NewReader([]byte(testutils.AddNewRow("") + testutils.AddNewRow(""))),
+				wr:      bytes.NewBuffer(nil),
+			},
+			args: args{
+				record:    &models.Record{Data: models.Data{RecordType: models.TypeBinary, Binary: &models.Binary{Name: "saveFile_invalid_cryptor_key.txt", SecuredFileName: "9a30a503b2862c51c3c5acd7fbce2f1f784cf4658ccf8e87d5023a90c21c0714"}}},
+				fileBytes: []byte("0sAy8Kq2v5AQtBaeGXahfBV1lPpzp3Ob4HITwxMARgs="),
+			},
+			want: want{
+				response:  nil,
+				fileBytes: nil,
+				err:       assert.Error,
+			},
+		},
+		{
+			name: "invalid hasher key",
+			fields: fields{
+				cryptor: ska.NewSKA("pass", ska.Key16),
+				hash:    hasher.CreateHasher("wrong", hasher.TypeSHA256, logger.CreateMock()),
+				rd:      bytes.NewReader([]byte(testutils.AddNewRow("") + testutils.AddNewRow(""))),
+				wr:      bytes.NewBuffer(nil),
+			},
+			args: args{
+				record:    &models.Record{Data: models.Data{RecordType: models.TypeBinary, Binary: &models.Binary{Name: "saveFile_invalid_hasher_key.txt", SecuredFileName: "9a30a503b2862c51c3c5acd7fbce2f1f784cf4658ccf8e87d5023a90c21c0714"}}},
+				fileBytes: []byte("0sAy8Kq2v5AQtBaeGXahfBV1lPpzp3Ob4HITwxMARgs="),
+			},
+			want: want{
+				response:  nil,
+				fileBytes: nil,
+				err:       assert.Error,
+			},
+		},
+		{
+			name: "invalid protected file bytes",
+			fields: fields{
+				cryptor: ska.NewSKA("pass", ska.Key16),
+				hash:    hasher.CreateHasher("", hasher.TypeSHA256, logger.CreateMock()),
+				rd:      bytes.NewReader([]byte(testutils.AddNewRow("") + testutils.AddNewRow(""))),
+				wr:      bytes.NewBuffer(nil),
+			},
+			args: args{
+				record:    &models.Record{Data: models.Data{RecordType: models.TypeBinary, Binary: &models.Binary{Name: "saveFile_invalid_hasher_key.txt", SecuredFileName: "9a30a503b2862c51c3c5acd7fbce2f1f784cf4658ccf8e87d5023a90c21c0714"}}},
+				fileBytes: []byte("0sAy8Kq2v5AQtBaeGXahfBV1lPpzp3Ob4HITwxRgs="),
+			},
+			want: want{
+				response:  nil,
+				fileBytes: nil,
+				err:       assert.Error,
+			},
+		},
+	}
+	for _, tt := range tests {
+		// TODO: make parallel (different files to use).
+		t.Run(tt.name, func(t *testing.T) {
+			iactr := testutils.CreateUserInteractor(tt.fields.rd, tt.fields.wr, logger.CreateMock())
+			b := &Binary{
+				iactr:     iactr,
+				sm:        tt.fields.sm,
+				hash:      tt.fields.hash,
+				cryptor:   tt.fields.cryptor,
+				storePath: tt.fields.storePath,
+			}
+
+			assert.NoError(t, os.WriteFile(tt.args.record.Data.Binary.SecuredFileName, tt.args.fileBytes, 0o666))
+			defer func() { // TODO: bad idea to call in cycle.
+				assert.NoError(t, os.Remove(tt.args.record.Data.Binary.SecuredFileName))
+			}()
+
+			err := b.saveFile(tt.args.record, tt.args.pathToFile)
+			tt.want.err(t, err)
+			if err != nil {
+				return
+			}
+
+			writtenBytes, err := os.ReadFile(tt.args.record.Data.Binary.Name)
+			assert.NoError(t, err)
+			defer func() { // TODO: bad idea to call in cycle.
+				assert.NoError(t, os.Remove(tt.args.record.Data.Binary.Name))
+			}()
+			assert.Equal(t, tt.want.fileBytes, writtenBytes)
+			assert.Equal(t, tt.want.response, tt.fields.wr.Bytes())
 		})
 	}
 }
